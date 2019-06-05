5e7de4abb773008c19a27fc224d132e7a5eaa87e
SeonYoungKim98 <kimksy9@gmail.com>
2019-06-05 19:51:05 +0900

    06_05

--- app/src/main/java/com/smu/fragment_viewpaper_prac/MainActivity.kt
+++ app/src/main/java/com/smu/fragment_viewpaper_prac/MainActivity.kt
@@ -5,27 +5,100 @@ import android.os.Bundle
 import android.support.v4.view.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
 import com.smu.fragment_viewpaper_prac.R.*
+import android.support.v4.app.ActivityCompat.startActivityForResult
+import android.content.Intent
+import android.support.v4.app.ActivityCompat
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val adapter by lazy { MainAdapter(supportFragmentManager)}
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        vpMainActivity.adapter = MainActivity@adapter
-        vpMainActivity.addOnPageChangeListener(object: ViewPager.OnPageChangeListener{
 
-            override fun onPageScrollStateChanged(p0: Int) {
-            }
-            override fun onPageScrolled(p0: Int, p1:Float, p2:Int){
-            }
-            override fun onPageSelected(p0: Int) {
-                ciMainActivity.selectDot(p0)
-            }
-        })
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
 
-        ciMainActivity.createDotPanel(3,drawable.indicator_dot_off,drawable.indicator_dot_on,0)
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+        auth = FirebaseAuth.getInstance()
+
+        btn_login.setOnClickListener {
+            signIn()
+        }
+
+        textView.text=auth.currentUser?.email
+    }
+
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // ...
+            }
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+
+                    val user = auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+
+                }
+
+            }
+    }
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
     }
 }
-
